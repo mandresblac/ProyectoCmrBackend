de9a0a756ee8231309d0becf030fcaefdcb2ff53
manuel blanco <mandresblac@gmail.com>
2024-03-01 12:21:40 -0500

    commit4 archivo cliente.route

--- src/routes/cliente.route.ts
+++ src/routes/cliente.route.ts
@@ -1,11 +1,12 @@
+import { check } from 'express-validator';
 
 // Ruta del Path: /api/v1/cliente
 
-import { validateFields } from '../middlewares/validate-fields';
 import { crearClientes, deleteCliente, getClientes, getUnCliente, updateCliente, updateEstadoCliente } from './../controllers/cliente.controller';
 
 // Archivo que contiene rutas
 import { Router } from "express";
+import { validateFields } from '../middlewares/validate-fields';
 
 
 const router = Router();
@@ -15,8 +16,8 @@ router.post("/",
 [
   check("nombre", "El nombre es obligatorio").not().isEmpty(),
   check("direccion", "La dorección es obligatoria").not().isEmpty(), 
-  check("email", "El email es obligatotio").not().isEmpty().isEmail(),
   check("telefono", "El telefono es obligatorio").not().isEmpty(),
+  check("email", "El email es obligatotio").not().isEmpty().isEmail(),
   check("tipoDocumento", "El tipo documento es obligatorio").not().isEmpty(),
   check("numeroDocumento", "El número documento es obligatorio").not().isEmpty(),
   validateFields
@@ -30,6 +31,4 @@ router.delete("/:id", deleteCliente);
 
 export default router;
 
-function check(arg0: string, arg1: string) {
-  throw new Error("Function not implemented.");
-}
+
